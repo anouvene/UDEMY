2df09cfcdd1d329314fb3cb72fa3b2e03faf1f6d
ANTOINE NOUVENE <nouvene@gmail.com>
2020-10-07 01:10:56 +0200

    [chap8-classes-et=techniques=utilitaires] tp-31 sur les classes wrappers

--- JAVA/JF/java-fondamentaux/chap8_classes-et-techniques-utilitaires/hello_universe/src/HelloUniverse.java
+++ JAVA/JF/java-fondamentaux/chap8_classes-et-techniques-utilitaires/hello_universe/src/HelloUniverse.java
@@ -28,6 +28,42 @@ public class HelloUniverse {
         PlaneteGazeuse neptune = new PlaneteGazeuse("Neptune");
         neptune.diametre = 49532;
 
+        // Atmosphere
+        Atmosphere atmosphereUranus = new Atmosphere();
+        atmosphereUranus.tauxHydrogene = new Float(83.0f);
+        atmosphereUranus.tauxHelium = new Float(15.0f);
+        atmosphereUranus.tauxMethane = new Float(2.5f);
+        atmosphereUranus.tauxAzote = new Float(0.0f);
+
+        uranus.atmosphere = atmosphereUranus;
+
+        System.out.println("Les constituants d'Uranus:");
+
+        if(uranus.atmosphere.tauxHelium != null) {
+            System.out.println("Taux d'hélium: " + uranus.atmosphere.tauxHelium + "%");
+        }
+
+        if(uranus.atmosphere.tauxHydrogene != null) {
+            System.out.println("Taux d'hydrogène: " + uranus.atmosphere.tauxHydrogene + "%");
+        }
+
+        if(uranus.atmosphere.tauxAzote != null) {
+            System.out.println("Taux d'azote: " + uranus.atmosphere.tauxAzote + "%");
+        }
+        if(uranus.atmosphere.tauxArgon != null) {
+            System.out.println("Taux d'argon: " + uranus.atmosphere.tauxArgon + "%");
+        }
+        if(uranus.atmosphere.tauxDioxydeDeCarbone != null) {
+            System.out.println("Taux de dioxyde de carbonne: " + uranus.atmosphere.tauxDioxydeDeCarbone + "%");
+        }
+        if(uranus.atmosphere.tauxSodium != null) {
+            System.out.println("Taux de sodium: " + uranus.atmosphere.tauxSodium + "%");
+        }
+        if(uranus.atmosphere.tauxMethane != null) {
+            System.out.println("Taux de méthane: " + uranus.atmosphere.tauxMethane + "%");
+        }
+
+
         // Scanner
         Scanner sc = new Scanner(System.in);
 
